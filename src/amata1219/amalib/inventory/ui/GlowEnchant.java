d5800be5af71bf74a990ed7e7f8c0cd84c164a78
amata1219 <suzukiamata@gmail.com>
2019-07-15 13:12:35 +0900

    こみっとー

---
+++ src/amata1219/amalib/inventory/ui/GlowEnchant.java
@@ -0,0 +1,58 @@
+package amata1219.amalib.inventory.ui;
+
+import org.bukkit.NamespacedKey;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.enchantments.EnchantmentTarget;
+import org.bukkit.inventory.ItemStack;
+
+public class GlowEnchant extends Enchantment {
+
+	public static void register(){
+		Enchantment.registerEnchantment(new GlowEnchant());
+	}
+
+	public GlowEnchant() {
+		super(NamespacedKey.minecraft("glow"));
+	}
+
+	@Override
+	public boolean canEnchantItem(ItemStack arg0) {
+		return false;
+	}
+
+	@Override
+	public boolean conflictsWith(Enchantment arg0) {
+		return false;
+	}
+
+	@Override
+	public EnchantmentTarget getItemTarget() {
+		return null;
+	}
+
+	@Override
+	public int getMaxLevel() {
+		return 0;
+	}
+
+	@Override
+	public String getName() {
+		return "glowing";
+	}
+
+	@Override
+	public int getStartLevel() {
+		return 0;
+	}
+
+	@Override
+	public boolean isCursed() {
+		return false;
+	}
+
+	@Override
+	public boolean isTreasure() {
+		return false;
+	}
+
+}
--- src/amata1219/amalib/inventory/ui/Icon.java
+++ src/amata1219/amalib/inventory/ui/Icon.java
@@ -1,5 +1,32 @@
 package amata1219.amalib.inventory.ui;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+
+import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.inventory.ItemFlag;
+import org.bukkit.inventory.ItemStack;
+
+
 public class Icon {
+
+	private final ItemStack base;
+	private final Material material;
+	private final int amount;
+	private final short durability;
+	private final String name;
+	private final ArrayList<String> lore = new ArrayList<>();
+	private final HashMap<Enchantment, Integer> enchantments = new HashMap<>();
+	private final HashSet<ItemFlag> flags = new HashSet<>();
+
+	public ItemStack toItemStack(){
+
+	}
+
+	public void glow(){
+		enchantments.put(new GlowEnchant(), 1);
+	}
 
 }
