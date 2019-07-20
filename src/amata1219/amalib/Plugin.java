47ed7ed75a8f5924a480646c465dd7b22bcf580b
amata1219 <suzukiamata@gmail.com>
2019-07-21 03:31:00 +0900

    こみっとー

---
+++ src/amata1219/amalib/Plugin.java
@@ -0,0 +1,47 @@
+package amata1219.amalib;
+
+import java.util.HashMap;
+
+import org.bukkit.command.CommandSender;
+import org.bukkit.event.HandlerList;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import amata1219.amalib.command.Arguments;
+import amata1219.amalib.command.Command;
+import amata1219.amalib.command.Sender;
+
+public class Plugin extends JavaPlugin {
+
+	private final HashMap<String, Command> commands = new HashMap<>();
+
+	@Override
+	public void onDisable(){
+		HandlerList.unregisterAll((JavaPlugin) this);
+	}
+
+	@Override
+	public boolean onCommand(CommandSender sender, org.bukkit.command.Command command, String label, String[] args){
+		commands.get(command.getName()).onCommand(new Sender(sender), new Arguments(args));
+		return true;
+	}
+
+	protected void registerCommands(Command... commands){
+		for(Command command : commands){
+			//コマンドのクラス名を取得する
+			String className = command.getClass().getSimpleName();
+
+			//接尾辞のCommandを削除し小文字化した物をコマンド名とする
+			String commandName = className.substring(0, className.length() - 7).toLowerCase();
+
+			//コマンド名とコマンドを結び付けて登録する
+			this.commands.put(commandName, command);
+		}
+	}
+
+	protected void registerListeners(Listener... listeners){
+		for(Listener listener : listeners)
+			getServer().getPluginManager().registerEvents(listener, this);
+	}
+
+}
