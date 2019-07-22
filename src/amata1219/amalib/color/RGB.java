e6a281362a802e7f0667a99a1b394f5f78007927
amata1219 <suzukiamata@gmail.com>
2019-07-22 19:44:11 +0900

    こみっとー

--- src/amata1219/amalib/color/RGB.java
+++ src/amata1219/amalib/color/RGB.java
@@ -2,16 +2,16 @@ package amata1219.amalib.color;
 
 public class RGB {
 
-	public final int red, blue, green;
-	public final double redParcent, blueParcent, greenParcent;
+	public final int red, green, blue;
+	public final double redParcent, greenParcent, blueParcent;
 
-	public RGB(int red, int blue, int green){
+	public RGB(int red, int green, int blue){
 		this.red = Math.min(Math.max(red, 0), 255);
-		this.blue = Math.min(Math.max(red, 0), 255);
 		this.green = Math.min(Math.max(red, 0), 255);
+		this.blue = Math.min(Math.max(red, 0), 255);
 		redParcent = red / 255D;
-		greenParcent = blue / 255D;
 		blueParcent = green / 255D;
+		greenParcent = blue / 255D;
 	}
 
 }
