824bfd48b2182f815cc727547678c80a72e3a45b
amata1219 <suzukiamata@gmail.com>
2019-07-28 10:14:12 +0900

    こみっとー

---
+++ src/amata1219/amalib/text/StringJoin.java
@@ -0,0 +1,29 @@
+package amata1219.amalib.text;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class StringJoin {
+
+	public static <T> String join(T[] objects){
+		return join(objects, "");
+	}
+
+	public static <T> String join(T[] objects, String delimiter){
+		return join(Arrays.stream(objects).map(Object::toString), delimiter);
+	}
+
+	public static String join(int[] objects){
+		return join(objects, "");
+	}
+
+	public static String join(int[] objects, String delimiter){
+		return join(Arrays.stream(objects).mapToObj(String::valueOf), delimiter);
+	}
+
+	private static String join(Stream<String> stream, String delimiter){
+		return stream.collect(Collectors.joining(delimiter));
+	}
+
+}
