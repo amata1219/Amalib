c4b064c9fc25bb5c9b186a29df8993fd6c9a20ef
amata1219 <suzukiamata@gmail.com>
2019-08-01 02:50:12 +0900

    こみっとー

---
+++ src/amata1219/amalib/item/skull/SkullMaker.java
@@ -0,0 +1,39 @@
+package amata1219.amalib.item.skull;
+
+import java.util.UUID;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.OfflinePlayer;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.SkullMeta;
+
+import amata1219.amalib.text.StringTemplate;
+
+public class SkullMaker {
+
+	private static final ItemStack BASE_SKULL = new ItemStack(Material.PLAYER_HEAD);
+
+	public static ItemStack fromPlayerUniqueId(UUID uuid){
+		return fromOfflinePlayer(Bukkit.getOfflinePlayer(uuid));
+	}
+
+	public static ItemStack fromOfflinePlayer(OfflinePlayer player){
+		ItemStack skull = BASE_SKULL.clone();
+
+		SkullMeta meta = (SkullMeta) skull.getItemMeta();
+		meta.setOwningPlayer(player);
+
+		skull.setItemMeta(meta);
+
+		return skull;
+	}
+
+	@SuppressWarnings("deprecation")
+	public static ItemStack fromBase64(String base64){
+		int hash = base64.hashCode();
+		UUID uuid = new UUID(hash, hash);
+		return Bukkit.getUnsafe().modifyItemStack(BASE_SKULL.clone(), StringTemplate.format("{SkullOwner:{Id:\"$0\",Properties:{textures:[{Value:\"$1\"}]}}}", uuid, base64));
+	}
+
+}
