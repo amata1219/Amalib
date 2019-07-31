91b9b84f2a20d87e17a9f205797e97c2f3553412
amata1219 <suzukiamata@gmail.com>
2019-07-31 13:19:41 +0900

    こみっとー

--- src/amata1219/amalib/selection/RegionSelectionSet.java
+++
@@ -1,9 +0,0 @@
-package amata1219.amalib.selection;
-
-import java.util.HashMap;
-
-public class RegionSelectionSet<K> {
-
-	public final HashMap<K, RegionSelection> selections = new HashMap<>();
-
-}
