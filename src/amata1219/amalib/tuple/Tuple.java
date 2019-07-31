a06c0079dd2a9a727bddd13bdd938871a28a419c
amata1219 <suzukiamata@gmail.com>
2019-07-31 12:19:15 +0900

    こみっとー

---
+++ src/amata1219/amalib/tuple/Tuple.java
@@ -0,0 +1,20 @@
+package amata1219.amalib.tuple;
+
+import amata1219.amalib.text.StringTemplate;
+
+public class Tuple<F, S> {
+
+	public final F first;
+	public final S second;
+
+	public Tuple(F first, S second){
+		this.first = first;
+		this.second = second;
+	}
+
+	@Override
+	public String toString(){
+		return StringTemplate.format("Tuple(first = $0, second = $1)", first, second);
+	}
+
+}
