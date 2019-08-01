d7a27be1a5bbe1d261a9257c7079e2a95281464c
amata1219 <suzukiamata@gmail.com>
2019-08-02 02:11:43 +0900

    こみっとー

---
+++ src/amata1219/amalib/tuplet/Quadruple.java
@@ -0,0 +1,24 @@
+package amata1219.amalib.tuplet;
+
+import amata1219.amalib.text.StringTemplate;
+
+public class Quadruple<F, S, T, FO> {
+
+	public final F first;
+	public final S second;
+	public final T third;
+	public final FO fourth;
+
+	public Quadruple(F first, S second, T third, FO fourth){
+		this.first = first;
+		this.second = second;
+		this.third = third;
+		this.fourth = fourth;
+	}
+
+	@Override
+	public String toString(){
+		return StringTemplate.format("Tuple(first = $0, second = $1, third = $2, fourth = $3)", first, second, third, fourth);
+	}
+
+}
