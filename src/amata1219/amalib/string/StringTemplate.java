3d95891a6132890577e75a5d94b592dcb92f6921
amata1219 <suzukiamata@gmail.com>
2019-08-08 18:53:43 +0900

    こみっとー

--- src/amata1219/amalib/string/StringTemplate.java
+++ src/amata1219/amalib/string/StringTemplate.java
@@ -3,12 +3,12 @@ package amata1219.amalib.string;
 public class StringTemplate {
 
 	public static String apply(String text, Object... objects){
-		return apply(text, "$", objects);
+		return apply(text, '$', objects);
 	}
 
-	public static String apply(String text, String alternateCode, Object... objects){
+	public static String apply(String text, char alternateCode, Object... objects){
 		for(int i = 0; i < objects.length; i++)
-			text = text.replace(alternateCode + i, objects[i].toString());
+			text = text.replace(alternateCode + String.valueOf(i), objects[i].toString());
 
 		return text;
 	}
