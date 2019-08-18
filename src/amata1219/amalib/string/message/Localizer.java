8c59771334c28c8476de7bf29f1cb96ea63b15a4
amata1219 <suzukiamata@gmail.com>
2019-08-18 17:18:51 +0900

    こみっとー

--- src/amata1219/amalib/string/message/Localizer.java
+++ src/amata1219/amalib/string/message/Localizer.java
@@ -16,16 +16,32 @@ public class Localizer {
 		return StringLocalize.localize(text, player);
 	}
 
+	public Message mlocalize(String text){
+		return Message.wrap(localize(text));
+	}
+
 	public String template(String text, Object... objects){
 		return StringLocalize.template(text, player, objects);
+	}
+
+	public Message mtemplate(String text, Object... objects){
+		return Message.wrap(template(text, objects));
 	}
 
 	public String color(String text){
 		return StringLocalize.color(text, player);
 	}
 
+	public Message mcolor(String text){
+		return Message.wrap(color(text));
+	}
+
 	public String applyAll(String text, Object... objects){
 		return StringLocalize.applyAll(text, player, objects);
+	}
+
+	public Message mapplyAll(String text, Object... objects){
+		return Message.wrap(applyAll(text, objects));
 	}
 
 }
